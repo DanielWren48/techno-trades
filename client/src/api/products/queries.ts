1a3f8ee6642590f3842d61f3fbd1dfc6d1346192
Dan <daniel.wren38@proton.me>
2025-02-13 23:47:47 +0000

    added: (client) api function to create a product

--- client/src/api/products/queries.ts
+++ client/src/api/products/queries.ts
@@ -1,5 +1,8 @@
+import { INewProduct, Product } from '@/types';
 import { authApi } from './requests';
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { BaseShopResponse } from './types';
+import { ErrorResponse } from 'react-router-dom';
 
 enum QUERY_KEYS {
     GET_PRODUCTS = "getProducts",
@@ -9,5 +12,17 @@ export const useGetProducts = () => {
     return useQuery({
         queryKey: [QUERY_KEYS.GET_PRODUCTS],
         queryFn: authApi.products,
+    });
+};
+
+export const useCreateNewProduct = () => {
+    return useMutation<BaseShopResponse<Product>, BaseShopResponse<ErrorResponse>, INewProduct>({
+        mutationFn: authApi.createProduct,
+        onSuccess: (response) => {
+            console.log({ response });
+        },
+        onError: (error) => {
+            console.error('Validation error:', error);
+        }
     });
 };
--- client/src/api/products/requests.ts
+++ client/src/api/products/requests.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { BaseShopResponse, AllProductsResponse } from './types';
+import { INewProduct, Product } from '@/types';
 
 // API client setup
 const api = axios.create({
@@ -26,6 +27,11 @@ export const authApi = {
     products: async (): Promise<BaseShopResponse<AllProductsResponse>> => {
         const response = await api.get<BaseShopResponse<AllProductsResponse>>('/products');
         // console.log(response.data.data)
+        return response.data;
+    },
+    createProduct: async (data: INewProduct): Promise<BaseShopResponse<Product>> => {
+        const response = await api.post<BaseShopResponse<Product>>('', data);
+        console.log(response)
         return response.data;
     },
 }
