e1a92b23b76db4ca139815122e77edcb71d053b8
Dan <daniel.wren38@proton.me>
2025-03-25 23:06:17 +0000

    refactor: (server) removed old/deprecated code from utils

--- server/src/utils/paginators.ts
+++ server/src/utils/paginators.ts
@@ -1,5 +1,5 @@
 import { Request } from "express";
-import { Document } from "mongoose";
+import { Document, Model } from "mongoose";
 
 interface PaginationResponse<T> {
     items?: T[];
@@ -7,6 +7,22 @@ interface PaginationResponse<T> {
     itemsCount: number;
     totalPages: number;
     itemsPerPage: number;
+}
+
+const paginateModel = async <T extends Document>(req: Request, modelClass: Model<T>, filter: Record<string, any> = {}, populateData: any, sortData: Record<string, any> = { createdAt: -1 }): Promise<PaginationResponse<T>> => {
+    // paginate data from the model
+    const page = parseInt(req.query.page as string) || 1; // Default to page 1
+    const limit = parseInt(req.query.limit as string) || 100; // Default to 100 items per page
+
+    const skip = (page - 1) * limit;
+
+    // Get total count of items
+    const itemsCount = await modelClass.countDocuments(filter);
+
+    // Get paginated items
+    const items = await modelClass.find(filter).populate(populateData).skip(skip).limit(limit).sort(sortData);
+    const totalPages = Math.ceil(itemsCount / limit)
+    return { items, page, itemsCount, totalPages, itemsPerPage: limit }
 }
 
 const paginateRecords = async <T extends Document>(req: Request, records: any[]): Promise<PaginationResponse<T>> => {
@@ -25,4 +41,4 @@ const paginateRecords = async <T extends Document>(req: Request, records: any[])
     return { items: paginatedItems, page, itemsCount, totalPages, itemsPerPage: limit }
 }
 
-export { paginateRecords }
+export { paginateModel, paginateRecords }
