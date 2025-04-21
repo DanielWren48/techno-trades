29dc1dc12fd06a22d10581a9df25b41885ca36cd
Dan <daniel.wren38@proton.me>
2025-04-21 23:30:55 +0100

    added: (server) new schema `category` to replace the old type string on product

---
+++ server/src/models/category.ts
@@ -0,0 +1,42 @@
+import slugify from 'slugify'
+import mongoose, { Schema, Document, Types } from 'mongoose';
+
+interface ICategory extends Document {
+    name: string;
+    slug: string;
+    icon: string;
+    image: string | null;
+    parent?: Types.ObjectId | ICategory | null;
+}
+
+const CategorySchema = new Schema<ICategory>({
+    name: { type: String, required: true, unique: true },
+    slug: { type: String },
+    icon: { type: String, required: true, unique: true },
+    image: { type: String, default: null, required: false },
+    parent: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        default: null
+    },
+}, { timestamps: true });
+
+CategorySchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('name') || this.isNew) {
+            const newSlug = slugify(this.name, { lower: true, strict: true });
+            this.slug = newSlug;
+        }
+        next();
+    } catch (error: any) {
+        next(error);
+    }
+});
+
+CategorySchema.methods.getChildren = async function (): Promise<ICategory[]> {
+    return await mongoose.model('Category').find({ parent: this._id });
+};
+
+const Category = mongoose.model<ICategory>('Category', CategorySchema);
+
+export { ICategory, Category };
